39088888358187a344fd6daa60706e0e4e605a99
Khalid12Clover <92978759+Khalid12Clover@users.noreply.github.com>
2025-07-23 14:43:01 +0530

    Update dnchk.js

--- dnchk.js
+++ dnchk.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 // Create a connection to the MySQL database
 const connection = mysql.createConnection({
-    host: "192.168.180.230",
+    host: "192.168.9.100",
     user: "root",
     password: "Root@123",
     database: 'pharmacy'
