8d52473037e5e2ff218777b181fc81601a3142a6
Khalid12Clover <92978759+Khalid12Clover@users.noreply.github.com>
2025-07-23 14:22:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var expressSession = require('express-session');
 var db = require('./db');
 //var hbs = require('express-handlebars');
 var path = require('path');
-var mysql = require('mysql2');
+var mysql = require('mysql');
 var async = require('async');
 
 var admin = require('./routes/admin');
