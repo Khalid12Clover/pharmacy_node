bd183b09c7d2bccc06f3d99a124a0e01dd745a64
Khalid12Clover <92978759+Khalid12Clover@users.noreply.github.com>
2025-11-04 14:28:10 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 module.exports = {
     getData: function(sql, param, callback){
         var connection = mysql.createConnection({
-            host: '192.168.9.100',
+            host: 'localhost:3306',
             user: 'root',
             password: 'Root@123',
             database: 'pharmacy'
